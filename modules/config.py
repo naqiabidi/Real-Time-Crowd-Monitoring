8256acee7a9265479352468d68d8280053103be2
naqiabidi <naqiabidi19@gmail.com>
2025-03-12 16:17:07 +0530

    update in file before pushing

--- modules/config.py
+++ modules/config.py
@@ -26,16 +26,16 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 PHOTOS_DIR = os.path.join(BASE_DIR, "photos")
 
 # Path to specific image
-NAQI_IMAGE_PATH = os.path.join(PHOTOS_DIR, "naqi.jpg")
+person1_IMAGE_PATH = os.path.join(PHOTOS_DIR, "your_image_file_name")
 
 # Face recognition setup
 known_faces_encoding = []
 known_faces_name = []
 
 # Example: Load known person image
-known_person1_image = face_recognition.load_image_file(NAQI_IMAGE_PATH)
+known_person1_image = face_recognition.load_image_file(person1_IMAGE_PATH)
 known_person1_encoding = face_recognition.face_encodings(known_person1_image)[0]
 known_faces_encoding.append(known_person1_encoding)
-known_faces_name.append("Naqi")
+known_faces_name.append("person1")
 
 face_detection_enabled = False
